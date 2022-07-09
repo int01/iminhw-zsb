8ef00ecdd6d51b4555416ce0a1ab08c66ee89e37
凉伊 <iminhw@qq.com>
2022-07-09 17:25:41 +0800

    改正vm模版引用错误

--- iminhw-archives/src/main/java/com/minhw/archives/domain/RemainArchives.java
+++ iminhw-archives/src/main/java/com/minhw/archives/domain/RemainArchives.java
@@ -59,15 +59,15 @@ public class RemainArchives extends BaseEntity {
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "更新时间" , width = 30, dateFormat = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "更新时间" , width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date updateDate;
 
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "创建时间" , width = 30, dateFormat = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "创建时间" , width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date createDate;
 
     public Long getId() {
