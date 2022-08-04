0fd3715b58d5ffe2b5bd06419438696b61608148
凉伊 <iminhw@qq.com>
2022-08-05 04:10:52 +0800

    修改错误

--- iminhw-analysis/src/main/java/com/minhw/analysis/domain/AnalysisMatriculate.java
+++ iminhw-analysis/src/main/java/com/minhw/analysis/domain/AnalysisMatriculate.java
@@ -51,111 +51,16 @@ public class AnalysisMatriculate extends BaseEntity {
      * 投档成绩
      */
     @Excel(name = "最高分")
-    private Double maxTdcj;
+    private Double maxtdcj;
     @Excel(name = "最低分")
-    private Double minTdcj;
+    private Double mintdcj;
     @Excel(name = "平均分")
-    private Double avgTdcj;
+    private Double avgtdcj;
 
     /**
      * 人数
      */
     @Excel(name = "录取人数")
-    private Long countNum;
+    private Long countnum;
 
-    public String getNd() {
-        return nd;
-    }
-
-    public void setNd(String nd) {
-        this.nd = nd;
-    }
-
-    public String getDq() {
-        return dq;
-    }
-
-    public void setDq(String dq) {
-        this.dq = dq;
-    }
-
-    public Long getPc() {
-        return pc;
-    }
-
-    public void setPc(Long pc) {
-        this.pc = pc;
-    }
-
-    public Long getKl() {
-        return kl;
-    }
-
-    public void setKl(Long kl) {
-        this.kl = kl;
-    }
-
-    public String getZy() {
-        return zy;
-    }
-
-    public void setZy(String zy) {
-        this.zy = zy;
-    }
-
-    public String getZxmc() {
-        return zxmc;
-    }
-
-    public void setZxmc(String zxmc) {
-        this.zxmc = zxmc;
-    }
-
-    public Double getMaxTdcj() {
-        return maxTdcj;
-    }
-
-    public void setMaxTdcj(Double maxTdcj) {
-        this.maxTdcj = maxTdcj;
-    }
-
-    public Double getMinTdcj() {
-        return minTdcj;
-    }
-
-    public void setMinTdcj(Double minTdcj) {
-        this.minTdcj = minTdcj;
-    }
-
-    public Double getAvgTdcj() {
-        return avgTdcj;
-    }
-
-    public void setAvgTdcj(Double avgTdcj) {
-        this.avgTdcj = avgTdcj;
-    }
-
-    public Long getCountNum() {
-        return countNum;
-    }
-
-    public void setCountNum(Long countNum) {
-        this.countNum = countNum;
-    }
-
-    @Override
-    public String toString() {
-        return "AnalysisMatriculate{" +
-                "nd='" + nd + '\'' +
-                ", dq='" + dq + '\'' +
-                ", pc=" + pc +
-                ", kl=" + kl +
-                ", zy='" + zy + '\'' +
-                ", zxmc='" + zxmc + '\'' +
-                ", maxTdcj=" + maxTdcj +
-                ", minTdcj=" + minTdcj +
-                ", avgTdcj=" + avgTdcj +
-                ", countNum=" + countNum +
-                '}';
-    }
 }
